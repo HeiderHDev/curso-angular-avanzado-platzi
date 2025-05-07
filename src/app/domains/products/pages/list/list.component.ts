e2ec7fb187373b21cc946ecc73eb41785d10d789
HeiderHDev <heiderhrnndz@gmail.com>
2025-05-06 23:39:55 -0500

    refactor: improve code formatting and structure across multiple components
    - Refactored HTML structure in About, Not Found, Product, and Product Detail components for better readability.
    - Updated TypeScript files to enhance code consistency and clarity, including proper use of signals and lifecycle hooks.
    - Improved the layout and styling in shared components like Header and Layout.
    - Enhanced the Search component with better input handling.
    - Cleaned up service files for categories and products, ensuring consistent formatting.
    - Adjusted Tailwind CSS configuration for better content management.
    - Fixed minor issues in various models and pipes for improved functionality.

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
+import {
+  Component,
+  Input,
+  inject,
+  signal,
+  OnInit,
+  OnChanges,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLinkWithHref } from '@angular/router';
 import { ProductComponent } from '@products/components/product/product.component';
@@ -10,16 +17,16 @@ import { CategoryService } from '@shared/services/category.service';
 import { Category } from '@shared/models/category.model';
 
 @Component({
-    selector: 'app-list',
-    imports: [CommonModule, ProductComponent, RouterLinkWithHref],
-    templateUrl: './list.component.html'
+  selector: 'app-list',
+  imports: [CommonModule, ProductComponent, RouterLinkWithHref],
+  templateUrl: './list.component.html',
 })
-export default class ListComponent {
-
+export default class ListComponent implements OnInit, OnChanges {
   products = signal<Product[]>([]);
   categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+
   private categoryService = inject(CategoryService);
   @Input() category_id?: string;
 
@@ -27,35 +34,33 @@ export default class ListComponent {
     this.getCategories();
   }
 
-  ngOnChanges(changes: SimpleChanges) {
+  ngOnChanges() {
     this.getProducts();
   }
 
   addToCart(product: Product) {
-    this.cartService.addToCart(product)
+    this.cartService.addToCart(product);
   }
 
   private getProducts() {
-    this.productService.getProducts(this.category_id)
-    .subscribe({
+    this.productService.getProducts(this.category_id).subscribe({
       next: (products) => {
         this.products.set(products);
       },
-      error: () => {
-        
-      }
-    })
+      error: (error) => {
+        console.log(error);
+      },
+    });
   }
 
   private getCategories() {
-    this.categoryService.getAll()
-    .subscribe({
+    this.categoryService.getAll().subscribe({
       next: (data) => {
         this.categories.set(data);
       },
-      error: () => {
-        
-      }
-    })
+      error: (error) => {
+        console.log(error);
+      },
+    });
   }
 }
